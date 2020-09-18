2778e444b5677f7487761c6565fcd3a140d4f9b9
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-09-18 21:51:44 +0700

    add router link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.scss';
 import LoginPage from './pages/login/LoginPage';
+import SignupPage from './pages/signup/SignupPage';
 import './styles/tailwind.output.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <Switch>
+        <Route exact path='/' component={LoginPage} />
+        <Route path='/signup' component={SignupPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Button from '../../components/Button/Button';
 import { Form, Input, Typography } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
 import 'antd/dist/antd.css';
 
 import './LoginPage.scss';
@@ -13,61 +15,65 @@ const LoginPage = () => {
   };
 
   return (
-    <Form
-      name='normal_login'
-      className='login-form bg-white shadow-md rounded px-10 pt-10 pb-20 m-auto'
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Title level={2}>Sign in</Title>
-      <Form.Item
-        name='username'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Email!',
-          },
-        ]}
+    <div>
+      <Form
+        name='normal_login'
+        className='login-form bg-white shadow-md rounded px-10 pt-10 pb-20 m-auto mt-20'
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input
-          prefix={<UserOutlined className='site-form-item-icon' />}
-          placeholder='Email'
-          bordered={false}
-          className='inputField emailInput'
-        />
-      </Form.Item>
-      <Form.Item
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className='site-form-item-icon' />}
-          type='password'
-          placeholder='Password'
-          bordered={false}
-          className='inputField passwordInput'
-        />
-      </Form.Item>
-      <Form.Item>
-        <a className='login-form-forgot' href='#forgot'>
-          Forgot password
-        </a>
-      </Form.Item>
+        <Title level={2}>Sign in</Title>
+        <Form.Item
+          name='username'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className='site-form-item-icon' />}
+            placeholder='Email'
+            bordered={false}
+            className='inputField emailInput'
+          />
+        </Form.Item>
+        <Form.Item
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className='site-form-item-icon' />}
+            type='password'
+            placeholder='Password'
+            bordered={false}
+            className='inputField passwordInput'
+          />
+        </Form.Item>
+        <Form.Item>
+          <a className='login-form-forgot' href='#forgot'>
+            Forgot password
+          </a>
+        </Form.Item>
 
-      <Form.Item>
-        <div className='btnLogin'>
-          <Button type='submit' btnName='Signin' />
-        </div>
-        Or <a href='register'>register now!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <div className='btnLogin'>
+            <Button type='submit' btnName='Signin' />
+          </div>
+        </Form.Item>
+      </Form>
+      <div className='mt-5'>
+        Or <Link to={`/signup`}>Register</Link>
+      </div>
+    </div>
   );
 };
 
